1fea9e9f58f501c70e1dc0d535cbd928cb3e5021
Parveen Dala <dparveen94@gmail.com>
2019-07-24 23:14:44 +0530

    Singleton design pattern

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,33 @@
+package singleton;
+
+/*******
+ * Parveen D
+ * Design-Pattern
+ */
+class Singleton {
+    private static volatile Singleton INSTANCE;
+
+    private Singleton() {
+        System.out.println("Instance Created");
+    }
+
+    public static Singleton getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Singleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new Singleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(Singleton::getInstance);
+        Thread t2 = new Thread(Singleton::getInstance);
+        t1.start();
+        t2.start();
+    }
+}
