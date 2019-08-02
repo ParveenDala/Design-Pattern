66d39f32a2353adaee7771c8027dd66dbbbf7a8c
Parveen Dala <dparveen94@gmail.com>
2019-08-03 01:34:02 +0530

    Add main class, test city and rubber duck

--- src/behavioural/strategy/Main.java
+++ src/behavioural/strategy/Main.java
@@ -5,4 +5,16 @@ package behavioural.strategy;
  * Strategy Pattern
  */
 public class Main {
+    public static void main(String[] args) {
+        Duck cityDuck = new Duck(new DefaultQuack(), new DefaultDisplay(), new DefaultFly());
+        cityDuck.display();
+        cityDuck.quack();
+        cityDuck.fly();
+
+        System.out.println();
+        Duck rubberDuck = new Duck(new DoNotQuack(), new RubberDisplay(), new DoNotFly());
+        rubberDuck.display();
+        rubberDuck.quack();
+        rubberDuck.fly();
+    }
 }
