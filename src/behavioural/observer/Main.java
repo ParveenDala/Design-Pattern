3ca69527650bc65d1d7f211d003feab6e04cdd06
Parveen Dala <31188015+ParveenDala@users.noreply.github.com>
2019-08-05 22:48:23 +0530

    Update Main.java

--- src/behavioural/observer/Main.java
+++ src/behavioural/observer/Main.java
@@ -6,7 +6,7 @@ package behavioural.observer;
  */
 public class Main {
     public static void main(String[] args) {
-        WeatherObservable weatherStation = new WeatherStation();
+        WeatherObservable weatherStation2 = new WeatherStation();
         weatherStation.add(new Mobile(weatherStation));
         weatherStation.add(new Tv(weatherStation));
     }
